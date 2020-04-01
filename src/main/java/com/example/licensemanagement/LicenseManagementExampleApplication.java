7685a9ee07d05ce246a238434ee8d3add560b932
Patranto Bagus Prabowo <prabowo@Patrantos-MacBook-Pro.local>
2020-04-01 15:54:46 +0700

    initialize repository

---
+++ src/main/java/com/example/licensemanagement/LicenseManagementExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.licensemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseManagementExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseManagementExampleApplication.class, args);
+	}
+
+}
